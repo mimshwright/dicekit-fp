cdfcf610b6cf57c1e65b0f23f434c99aecc8c490
Mims Wright <mims@mimswright.com>
2020-10-19 23:26:42 -0700

    added createDie() and others

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+type Unary<Param, Return> = (_: Param) => Return;
+
 const r = Math.random;
 
 export const randomNumberBetween = (high: number, low = 0): number =>
@@ -5,3 +7,14 @@ export const randomNumberBetween = (high: number, low = 0): number =>
 
 export const randomIntegerBetween = (high: number, low = 0): number =>
   Math.round(randomNumberBetween(high, low));
+
+// Unary version of randomIntegerBetween
+export const randomInteger: Unary<number, number> = randomIntegerBetween;
+
+export const randomBoolean = (): boolean => r() > 0.5;
+
+export const randomElement: <T>(arr: T[]) => T = (arr) =>
+  arr[randomInteger(arr.length - 1)];
+
+export const createDie: Unary<number, () => number> = (sides: number) => () =>
+  randomIntegerBetween(sides, 1);
--- test/blah.test.ts
+++ test/blah.test.ts
@@ -1,4 +1,9 @@
-import { randomIntegerBetween } from "../src";
+import {
+  randomBoolean,
+  createDie,
+  randomIntegerBetween,
+  randomInteger,
+} from "../src/index";
 
 const callMultipleTimes = (times: number) => (func: () => any) =>
   new Array(times).fill(0).map(func);
@@ -11,7 +16,7 @@ const min = (nums: number[]) =>
 
 // Test the test utils.
 
-describe("testUtils", () => {
+describe("test utils", () => {
   describe("callMultipleTimes()", () => {
     let x = 0;
     const testFunc = () => {
@@ -39,37 +44,76 @@ describe("testUtils", () => {
 // Test the main library.
 
 describe("dicekit", () => {
-  describe("randomIntegerBetween()", () => {
-    it("Creates random numbers in a range.", () => {
-      const results = callMultipleTimes(500)(() =>
-        randomIntegerBetween(50, 10),
-      );
+  describe("[WARNING: In rare cases, these tests can fail due to random values. Please test again if this happens!]", () => {
+    describe("randomIntegerBetween()", () => {
+      it("Creates random numbers in a range.", () => {
+        const results = callMultipleTimes(500)(() =>
+          randomIntegerBetween(50, 10),
+        );
 
-      expect(min(results)).toBe(10);
-      expect(max(results)).toBe(50);
+        expect(min(results)).toBe(10);
+        expect(max(results)).toBe(50);
+      });
+
+      it("Low value defaults to 0.", () => {
+        const results = callMultipleTimes(100)(() => randomIntegerBetween(10));
+
+        expect(min(results)).toBe(0);
+        expect(max(results)).toBe(10);
+      });
+
+      it("Negative numbers work.", () => {
+        const results = callMultipleTimes(500)(() =>
+          randomIntegerBetween(-10, -50),
+        );
+
+        expect(min(results)).toBe(-50);
+        expect(max(results)).toBe(-10);
+      });
+
+      it("Range is inclusive.", () => {
+        const results = callMultipleTimes(50)(() => randomIntegerBetween(1, 2));
+
+        expect(min(results)).toBe(1);
+        expect(max(results)).toBe(2);
+      });
     });
 
-    it("Low value defaults to 0.", () => {
-      const results = callMultipleTimes(100)(() => randomIntegerBetween(10));
+    describe("randomInteger()", () => {
+      it("Is a unary form of randomIntegerBetween()", () => {
+        const results = callMultipleTimes(100)(() => randomInteger(10));
+        const resultsBetween = callMultipleTimes(100)(() =>
+          randomIntegerBetween(10, 0),
+        );
 
-      expect(min(results)).toBe(0);
-      expect(max(results)).toBe(10);
+        expect(min(results)).toBe(min(resultsBetween));
+        expect(max(results)).toBe(max(resultsBetween));
+      });
+    });
+    describe("randomBoolean()", () => {
+      it("Is syntactic sugar for randomIntegerBetween(0,1) coerced to a boolean value", () => {
+        const results = callMultipleTimes(100)(randomBoolean);
+        const trueCount = results.filter((b) => b).length;
+
+        expect(trueCount).toBeGreaterThan(40);
+      });
     });
 
-    it("Negative numbers work.", () => {
-      const results = callMultipleTimes(500)(() =>
-        randomIntegerBetween(-10, -50),
-      );
+    describe("createDie()", () => {
+      it("Returns a function that generates random numbers between 1 and 'sides'", () => {
+        const d6 = createDie(6);
+        const results = callMultipleTimes(60)(d6);
 
-      expect(min(results)).toBe(-50);
-      expect(max(results)).toBe(-10);
-    });
+        expect(min(results)).toBe(1);
+        expect(max(results)).toBe(6);
+      });
+      it("Range is inclusive'", () => {
+        const coinFlip = createDie(2);
+        const results = callMultipleTimes(50)(coinFlip);
 
-    it("Range is inclusive.", () => {
-      const results = callMultipleTimes(50)(() => randomIntegerBetween(1, 2));
-
-      expect(min(results)).toBe(1);
-      expect(max(results)).toBe(2);
+        expect(min(results)).toBe(1);
+        expect(max(results)).toBe(2);
+      });
     });
   });
 });

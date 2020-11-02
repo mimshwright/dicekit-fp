8c26e168527661b79c868a0a9c8a572940075955
Mims Wright <mims@mimswright.com>
2020-11-02 12:06:08 -0800

    Added more type signatures to utils

--- src/utils.ts
+++ src/utils.ts
@@ -4,17 +4,23 @@ import { NumberGenerator, Modifier } from "./types";
 // eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/explicit-module-boundary-types
 export const identity = <T>(a: T) => (_ignored?: any) => a;
 
+// add :: Number a => (a,a) -> a
 export const add = (a: number, b: number): number => a + b;
+
+// sum :: Number a => (...a[]) -> a
 export const sum = (...numbers: number[]): number => numbers.reduce(add, 0);
+// sumArray :: Number a => a[] -> a
 export const sumArray = (numbers: number[]): number => sum(...numbers);
 
+// max :: Number a => a[] -> a
 export const max = (nums: number[]): number =>
   nums.reduce((a, b) => Math.max(a, b), Number.NEGATIVE_INFINITY);
 
+// min :: Number a => a[] -> a
 export const min = (nums: number[]): number =>
   nums.reduce((a, b) => Math.min(a, b), Number.POSITIVE_INFINITY);
 
-// callMultipleTimes :: Number t -> (t -> a) -> a[]
+// callMultipleTimes :: Number t => t -> (t -> a) -> a[]
 export const callMultipleTimes = (times: number) => (
   func: (index: number) => any,
 ): any[] => {
@@ -27,6 +33,7 @@ export const callMultipleTimes = (times: number) => (
   return results;
 };
 
+// callAndAdd :: Number a => a -> (() -> a) -> (() -> a)
 export const callAndAdd = (modifier: Modifier) => (
   func: NumberGenerator,
 ): NumberGenerator => () => func() + modifier;
